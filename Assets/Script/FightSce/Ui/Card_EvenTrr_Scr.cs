ac6648892c8e4768978bd04f7f739e5a5c4f49bc
LanMegumi <846196299@qq.com>
2018-12-11 16:30:51 +0800

    12-11-4
    卡牌的放大效果

--- Assets/Script/FightSce/Ui/Card_EvenTrr_Scr.cs
+++ Assets/Script/FightSce/Ui/Card_EvenTrr_Scr.cs
@@ -6,15 +6,31 @@ using UnityEngine.UI;
 
 
 public class Card_EvenTrr_Scr : MonoBehaviour {
-	RectTransform rect;
+	RectTransform rect,new_rect;
 	// Use this for initialization
 	void Start () {
 		rect = this.GetComponent<RectTransform>();
+		
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-
+	public void testFun(){
+		Debug.Log("rect.rect:"+rect.rect);
+		Debug.Log("rect.pivot:"+rect.pivot);
+		rect.sizeDelta=rect.sizeDelta*1.2f;
+		Debug.Log("rect.sizeDelta:"+rect.sizeDelta);
+		
+	}
+	public void testFun2(){
+		Debug.Log("rect.rect:"+rect.rect);
+		Debug.Log("rect.pivot:"+rect.pivot);
+		rect.sizeDelta=rect.sizeDelta/1.2f;
+		Debug.Log("rect.sizeDelta:"+rect.sizeDelta);
+		
+	}
 }
