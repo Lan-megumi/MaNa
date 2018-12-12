ef5efdca727d2f0ef03a5cbe9b58ece657c68ee7
Hsquan <877617346@qq.com>
2018-12-12 15:53:05 +0800

    12-12-2

--- Assets/Resources/Audio/Audios.cs
+++ Assets/Resources/Audio/Audios.cs
@@ -11,14 +11,24 @@ public class Audios : MonoBehaviour {
     
     // Use this for initialization
     public static Audios _instant;
+
+    
     private void Awake()
     {
         _instant = this;
     }
+
     void Start () {
        m_Audio = gameObject.GetComponent<AudioSource>();
-       m_Audio.clip= myMusicArray[1]; 
-
+       m_Audio.clip= myMusicArray[1];
+        m_Audio.volume = SoundSetting._instance.slider.value;
+        if (SoundSetting._instance.audioefbool == true)
+        {
+            m_Audio.mute = true;
+        }else
+        {
+            m_Audio.mute = false;
+        }
     }
 	
 	// Update is called once per frame
---
+++ Assets/Resources/Audio/SoundSetting.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SoundSetting : MonoBehaviour {
+    private GameObject Setting;
+
+    public GameObject isOnGameObject;  
+    public GameObject isOffGameObject;
+    public GameObject isOnGameObject1;
+    public GameObject isOffGameObject1;
+
+    private AudioSource m_audiobg;
+    private AudioSource m_audioef;
+
+    public bool audioefbool;
+    public Slider slider;
+    // Use this for initialization
+    public static SoundSetting _instance;
+    private void Awake()
+    {
+        _instance=this;
+    }
+
+    void Start () {
+        Setting = GameObject.Find("Setting");
+        Setting.SetActive(false);
+        
+        m_audiobg = GameObject.Find("Main Camera").GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Open()
+    {
+        Setting.SetActive(true);
+    }
+    public void Close()
+    {
+        Setting.SetActive(false);
+    }
+    //背景音乐
+    public void OnValueChange(bool isOn)
+    {
+        isOffGameObject.SetActive(isOn);
+        isOnGameObject.SetActive(!isOn);
+        if (isOn==true)
+        {
+            m_audiobg.mute = true;
+        } else
+        {
+            m_audiobg.mute = false;
+        }
+    }
+    //音效
+    public void OnValueChange1(bool isOn)
+    {
+        isOffGameObject1.SetActive(isOn);
+        isOnGameObject1.SetActive(!isOn);
+        if (isOn == true)
+        {
+            audioefbool = true;
+        }
+        else
+        {
+            audioefbool = false;
+        }
+        
+    }
+    public void SoundVolume()
+    {
+        m_audiobg.volume = slider.value;
+    }
+}
