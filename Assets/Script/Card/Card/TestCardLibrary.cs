740666f44861bb46d5221f00c27f4096b1f3f16b
Hsquan <877617346@qq.com>
2018-12-18 16:36:41 +0800

    12-18-3

--- Assets/Script/Card/Card/TestCardLibrary.cs
+++ Assets/Script/Card/Card/TestCardLibrary.cs
@@ -9,24 +9,45 @@ using UnityEngine.UI;
 public  class  TestCardLibrary:MonoBehaviour{
 	public static TestCardLibrary _instance;
 	TestCard EL01Fi = new TestCard("EL01Fi",(Type)0,(Element_type)0,(Rarity)0,"火花",1,25,(AttcakeType)0,0);
-	TestCard EL02Fi = new TestCard("EL02Fi",(Type)0,(Element_type)0,(Rarity)0,"燃烧",3,40,(AttcakeType)0,0);
-	TestCard EL03Fi = new TestCard("EL03Fi",(Type)0,(Element_type)0,(Rarity)1,"爆燃",6,55,(AttcakeType)4,0);
-	TestCard EL04Fi = new TestCard("EL04Fi",(Type)0,(Element_type)0,(Rarity)2,"幽炎",5,60,(AttcakeType)0,0);
-	TestCard EL05Fi = new TestCard("EL05Fi",(Type)0,(Element_type)0,(Rarity)2,"过热",7,0,(AttcakeType)0,0);
-	TestCard EL06Fi = new TestCard("EL06Fi",(Type)0,(Element_type)0,(Rarity)0,"赤焰",2,35,(AttcakeType)0,0);
-	TestCard EL07Fi = new TestCard("EL07Fi",(Type)0,(Element_type)0,(Rarity)1,"雷火",4,50,(AttcakeType)0,0);
-	TestCard EL08Fi = new TestCard("EL08Fi",(Type)0,(Element_type)0,(Rarity)2,"天罡焱",7,70,(AttcakeType)0,0);
-	TestCard EL09Fi = new TestCard("EL09Fi",(Type)0,(Element_type)0,(Rarity)2,"金灵律火",6,70,(AttcakeType)0,0);
-	TestCard EL01Wa=new TestCard("EL01Wa",(Type)0,(Element_type)1,(Rarity)0,"飞溅",2,30,(AttcakeType)4,0);
-	TestCard EL02Wa=new TestCard("EL02Wa",(Type)0,(Element_type)1,(Rarity)0,"清泉",4,25,(AttcakeType)2,40);
-	TestCard EL03Wa=new TestCard("EL03Wa",(Type)0,(Element_type)1,(Rarity)1,"凝结",4,30,(AttcakeType)0,0);
-	TestCard EL04Wa=new TestCard("EL04Wa",(Type)0,(Element_type)1,(Rarity)1,"暗潮",3,25,(AttcakeType)1,0);
-	TestCard EL01C1=new TestCard("EL01C1",(Type)0,(Element_type)2,(Rarity)0,"蔓延",4,10,(AttcakeType)3,0);
-	TestCard EL02C1=new TestCard("EL02C1",(Type)0,(Element_type)2,(Rarity)0,"轻盈",2,0,(AttcakeType)8,0);
-	TestCard EL03C1=new TestCard("EL03C1",(Type)0,(Element_type)2,(Rarity)0,"旋风",3,40,(AttcakeType)4,0);
-	TestCard EL04C1=new TestCard("EL04C1",(Type)0,(Element_type)2,(Rarity)3,"飓风迷域",6,30,(AttcakeType)1,0);
-	// public Text IdText,TypeText,EleText,RarityText,NameText;
-	public List<TestCard> Library0;
+	TestCard EL02Fi = new TestCard("EL02Fi",(Type)0,(Element_type)0,(Rarity)0,"灼烧",3,40,(AttcakeType)0,0);
+    TestCard EL03Fi = new TestCard("EL03Fi",(Type)0,(Element_type)0,(Rarity)0,"炽焰",2,40,(AttcakeType)0,0);
+    TestCard EL04Fi = new TestCard("EL04Fi",(Type)0,(Element_type)0,(Rarity)1,"爆燃",6,45,(AttcakeType)4,0);
+    TestCard EL05Fi = new TestCard("EL05Fi",(Type)0,(Element_type)0,(Rarity)1,"雷火",4,50,(AttcakeType)0,0);
+    TestCard EL06Fi = new TestCard("EL06Fi",(Type)0,(Element_type)0,(Rarity)1,"绯焰",4,45,(AttcakeType)0,0);
+    TestCard EL07Fi = new TestCard("EL07Fi",(Type)0,(Element_type)0,(Rarity)2,"幽炎",5,60,(AttcakeType)0,0);
+    TestCard EL08Fi = new TestCard("EL08Fi",(Type)0,(Element_type)0,(Rarity)2,"过热",4,50,(AttcakeType)0,0);
+    TestCard EL09Fi = new TestCard("EL09Fi",(Type)0,(Element_type)0,(Rarity)2,"天罡焱",7,70,(AttcakeType)0,0);
+    TestCard EL10Fi = new TestCard("EL10Fi",(Type)0,(Element_type)0,(Rarity)2,"雨焰",6,40,(AttcakeType)1,0);
+    TestCard EL11Fi = new TestCard("EL11Fi",(Type)0,(Element_type)0,(Rarity)3,"金灵律火",6,70,(AttcakeType)0,0);
+    TestCard EL12Fi = new TestCard("EL12Fi",(Type)0,(Element_type)0,(Rarity)3,"炼狱",8,70,(AttcakeType)1,0);
+    
+    TestCard EL01Wa=new TestCard("EL01Wa",(Type)0,(Element_type)1,(Rarity)0,"飞溅",2,30,(AttcakeType)4,0);
+    TestCard EL02Wa=new TestCard("EL02Wa",(Type)0,(Element_type)1,(Rarity)0,"清泉",4,25,(AttcakeType)2,40);
+    TestCard EL03Wa=new TestCard("EL03Wa",(Type)0,(Element_type)1,(Rarity)1,"凝结",4,30,(AttcakeType)0,0);
+    TestCard EL04Wa=new TestCard("EL04Wa",(Type)0,(Element_type)1,(Rarity)1,"暗潮",3,25,(AttcakeType)1,0);
+    TestCard EL05Wa=new TestCard("EL05Wa",(Type)0,(Element_type)1,(Rarity)1,"暴雪",4,30,(AttcakeType)1,0);
+    TestCard EL06Wa=new TestCard("EL06Wa",(Type)0,(Element_type)1,(Rarity)1,"翻涌",3,40,(AttcakeType)4,10);
+    TestCard EL07Wa=new TestCard("EL07Wa",(Type)0,(Element_type)1,(Rarity)1,"破浪",3,50,(AttcakeType)0,0);
+    TestCard EL08Wa=new TestCard("EL08Wa",(Type)0,(Element_type)1,(Rarity)2,"逝潮",4,30,(AttcakeType)1,0);
+    TestCard EL09Wa=new TestCard("EL09Wa",(Type)0,(Element_type)1,(Rarity)2,"碧涛",4,40,(AttcakeType)4,10);
+    TestCard EL10Wa=new TestCard("EL10Wa",(Type)0,(Element_type)1,(Rarity)2,"渗透",4,40,(AttcakeType)0,0);
+    TestCard EL11Wa=new TestCard("EL11Wa",(Type)0,(Element_type)1,(Rarity)3,"洛水",4,25,(AttcakeType)2,40);
+    TestCard EL12Wa=new TestCard("EL12Wa",(Type)0,(Element_type)1,(Rarity)3,"绝寒",8,70,(AttcakeType)1,0);
+
+    TestCard EL01C1=new TestCard("EL01C1",(Type)0,(Element_type)2,(Rarity)0,"蔓延",3,10,(AttcakeType)1,0);
+    TestCard EL02C1=new TestCard("EL02C1",(Type)0,(Element_type)2,(Rarity)0,"轻盈",2,15,(AttcakeType)8,0);
+    TestCard EL03C1=new TestCard("EL03C1",(Type)0,(Element_type)2,(Rarity)0,"悲风",3,40,(AttcakeType)4,0);
+    TestCard EL04C1=new TestCard("EL04C1",(Type)0,(Element_type)2,(Rarity)1,"氤氲",3,20,(AttcakeType)8,0);
+    TestCard EL05C1=new TestCard("EL05C1",(Type)0,(Element_type)2,(Rarity)1,"狂风",4,25,(AttcakeType)1,0);
+    TestCard EL06C1=new TestCard("EL06C1",(Type)0,(Element_type)2,(Rarity)1,"涡旋",3,50,(AttcakeType)0,0);
+    TestCard EL07C1=new TestCard("EL07C1",(Type)0,(Element_type)2,(Rarity)2,"气浪",4,70,(AttcakeType)0,10);
+    TestCard EL08C1=new TestCard("EL08C1",(Type)0,(Element_type)2,(Rarity)2,"风挽",4,40,(AttcakeType)4,20);
+    TestCard EL09C1=new TestCard("EL09C1",(Type)0,(Element_type)2,(Rarity)2,"风痕",4,60,(AttcakeType)0,0);
+    TestCard EL10C1=new TestCard("EL10C1",(Type)0,(Element_type)2,(Rarity)3,"迷域",6,30,(AttcakeType)1,0);
+    TestCard EL11C1=new TestCard("EL11C1",(Type)0,(Element_type)2,(Rarity)3,"苍穹",7,50,(AttcakeType)1,0);
+    TestCard EL12C1=new TestCard("EL12C1",(Type)0,(Element_type)2,(Rarity)3,"风语",6,55,(AttcakeType)1,0);
+    // public Text IdText,TypeText,EleText,RarityText,NameText;
+    public List<TestCard> Library0;
 	void Awake(){
 		_instance=this;
 	}
@@ -41,17 +62,38 @@ public  class  TestCardLibrary:MonoBehaviour{
 		Library0.Add(EL07Fi);
 		Library0.Add(EL08Fi);
 		Library0.Add(EL09Fi);
-		Library0.Add(EL01Wa);
+        Library0.Add(EL10Fi);
+        Library0.Add(EL11Fi);
+        Library0.Add(EL12Fi);
+
+        Library0.Add(EL01Wa);
 		Library0.Add(EL02Wa);
 		Library0.Add(EL03Wa);
 		Library0.Add(EL04Wa);
-		Library0.Add(EL01C1);
+        Library0.Add(EL05Wa);
+        Library0.Add(EL06Wa);
+        Library0.Add(EL07Wa);
+        Library0.Add(EL08Wa);
+        Library0.Add(EL09Wa);
+        Library0.Add(EL10Wa);
+        Library0.Add(EL11Wa);
+        Library0.Add(EL12Wa);
+
+        Library0.Add(EL01C1);
 		Library0.Add(EL02C1);
 		Library0.Add(EL03C1);
 		Library0.Add(EL04C1);
-		// Debug.Log("TestCardLib Star");
-        
-	}
+        Library0.Add(EL05C1);
+        Library0.Add(EL06C1);
+        Library0.Add(EL07C1);
+        Library0.Add(EL08C1);
+        Library0.Add(EL09C1);
+        Library0.Add(EL10C1);
+        Library0.Add(EL11C1);
+        Library0.Add(EL12C1);
+        // Debug.Log("TestCardLib Star");
+
+    }
 	
 	/*
 	洗牌功能
